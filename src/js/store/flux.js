90f46f6d67355f3ad78f2f18eed0f9441dadc34b
Yifan <yifanyemontpe@gmail.com>
2021-08-04 13:29:00 +0000

    Added fetch

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,33 @@
+import { useEffect } from "react";
+
 const getState = ({ getStore, getActions, setStore }) => {
+	const baseURL = "https://www.swapi.tech/api/";
+
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			starships: {}
 		},
+
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+			getStarships: () => {
+				fetch(baseURL.concat("starships?page=2&limit=100"), {
+					method: "GET",
+					mode: "cors",
+					redirect: "follow"
+				})
+					.then(resp => {
+						if (!resp.ok) {
+							throw Error(resp.statusText);
+						}
+						return resp.json();
+					})
+					.then(data => {
+						setStore({ starships: data.results });
+						console.log(data.results);
+					})
+					.catch(error => {
+						console.log(error);
+					});
 			}
 		}
 	};
