07d36e400ba14d5c774fd91b7cdffbe2d9f97b6b
Florence-Paloma <f.palomagaspar@gmail.com>
2021-08-04 14:14:59 +0000

    subir people

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,33 +1,30 @@
+import {useEfect} from "react";
+
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			people: {}		
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			getPeople:()=>{
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
+				fetch("https://www.swapi.tech/api/people?page=2&limit=100")
+				.then(response => {
+					if (!response.ok) {
+					throw new Error ("Something went wrong", response.status);
+					}
+					return response.json();				
+				})
+				.then(jsonPeople => {
+					setStore({people:jsonPeople.results})
+
+				})
+				.catch((error) => {
+					console.log('Looks like there was a problem: \n', error);
 				});
-
-				//reset the global store
-				setStore({ demo: demo });
 			}
 		}
-	};
-};
-
+	}
+}
+		
 export default getState;
