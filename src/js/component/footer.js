7a05fcafea6a013940a30ec8f272c7dc548456b0
Florence-Paloma <f.palomagaspar@gmail.com>
2021-08-14 19:24:18 +0000

    cambios finales de master más mi footer

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,38 +1,68 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import facebook from "./img/facebook.png";
-import insta from "./img/insta.png";
-import linkedin from "./img/linkedin.png";
+import Logo from "./img/logo.jpg";
+import { Button } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
 
 export const Footer = () => (
-	<div className="footer-basic">
-		<footer>
-			<div className="social">
-				<Link href="https://www.instagram.com/starwars/">
-					<img src={insta} className="social-media-icon" />
-				</Link>
-				<Link href="https://es-es.facebook.com/StarWars/">
-					<img src={facebook} className="social-media-icon" />
-				</Link>
-				<Link href="https://www.linkedin.com/showcase/starwars/about/">
-					<img src={linkedin} className="social-media-icon" />
-				</Link>
+	<footer className="footer mt-auto py-3 text-center">
+		<div className="container">
+			<div className="row">
+				{/*column1*/}
+				<div className="col">
+					<h4>Find us at 4Geeks Academy Spain</h4>
+					<img src="./logo.jpg" />
+				</div>
+				{/*column2*/}
+				<div className="col">
+					<p>MORE ABOUT US:</p>
+					<h4>
+						<ul className="list-unstyled">
+							<li>
+								<a href="https://github.com/YifanYes">
+									<i className="fab fa-github white" />
+								</a>{" "}
+								<a href="https://linkedin.com/in/yifan-ye-zhang-8042171b2/">
+									<i className="fab fa-linkedin" />
+								</a>{" "}
+								Yifan
+							</li>
+							<li>
+								<a href="https://github.com/BryanTello">
+									<i className="fab fa-github white" />
+								</a>{" "}
+								<a href="https://www.linkedin.com/in/bryan-bat-2a6987212/">
+									<i className="fab fa-linkedin" />
+								</a>{" "}
+								Bryan
+							</li>
+							<li>
+								<a href="https://github.com/Florence-Paloma">
+									<i className="fab fa-github white" />
+								</a>{" "}
+								<a href="https://www.linkedin.com/in/paloma-gaspar-rivera-73a96759/">
+									<i className="fab fa-linkedin" />
+								</a>{" "}
+								Paloma
+							</li>
+						</ul>
+					</h4>
+				</div>
+				{/*column3*/}
+				<div className="col">
+					<Form>
+						<Form.Group controlId="formBasicEmail">
+							<Form.Label>Subscribe to our Newsletter</Form.Label>
+							<Form.Control type="email" placeholder="Enter email" />
+							<Form.Text className="text-muted">
+								We will never share your email with anyone else.
+							</Form.Text>
+						</Form.Group>
+						<Button variant="primary" type="submit">
+							Subscribe Me!
+						</Button>
+					</Form>
+				</div>
 			</div>
-			<ul className="list-inline">
-				<li className="list-inline-item">
-					<Link href="#">Services</Link>
-				</li>
-				<li className="list-inline-item">
-					<Link href="#">About</Link>
-				</li>
-				<li className="list-inline-item">
-					<Link href="#">Terms</Link>
-				</li>
-				<li className="list-inline-item">
-					<Link href="#">Privacy Policy</Link>
-				</li>
-			</ul>
-			<p className="copyright">4Geeks Academy © 2021</p>
-		</footer>
-	</div>
+		</div>
+	</footer>
 );
